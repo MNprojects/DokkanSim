c16e3e3a2a84d3324aa17636cec8330f71ec576f
Tech-A67 <mnash@spartaglobal.com>
2018-06-26 21:49:29 +0100

    lots of changes

--- team_calculator.js
+++ team_calculator.js
@@ -1,29 +1,70 @@
 /*
+
 Dokkan Simulator Library
-Call 
+
+Call the method: 
 teamSim([ArrayContainingTeam], DesiredTurnSimCount)
-or 
+to run the simulator with the desired team and the amount of turns to simulate over.
 
 */
 
 // TODO: Add validation of current inputs
-// Returns a boolean for if the current inputs are valid
-function validateInputs(teamArray) {
-  if(teamArray.length !== 7){
-    return "The array is not the length required for the simulator. Requires: 7, Received: " + teamArray.length;
+
+// Validation functions. All return true if the given inputs are valid.
+function validateTeamArray(teamArray) {
+  if (teamArray.class === Array) {
+    return true;
   }
+}
+function validateTeamLength(teamArray) {
+  if (teamArray.length === 7) {
+    return true
+  } 
+}
+
+// Checks each character has the correct attributes
+function validateTeamCharacters(teamArray) {
+  errors = [];
   teamArray.forEach(card => {
-    if (card.turnStart()){
-       card.ki; 
-      }
+    if (!card.property){
+       errors.push([card.name, card.property]);
+    }
+    else if (card.otherPropETC != 0) {
+      errors.push([card.name, "otherPropETC" ,card.otherPropETC]);
+    }
   });
+  if (errors.length === 0){
+  return true;
+  }
+  else {
+    return errors;
+  }
+}
+
+function runValidations(teamArray){
+  if (!validateTeamArray(teamArray)) {
+    console.log("The team is not in the correct datastructure. Expected: Array, Received: " + teamArray.class);
+  }
+  if (!validateTeamLength(teamArray)) {
+    console.log("The array is not the length required for the simulator. Requires: 7, Received: " + teamArray.length);
+  }
+  errors = validateTeamCharacters(teamArray);
+  if (errors != true) {
+    console.log("There is something wrong with one or more of the characters given. Received: ");
+    for (let i = 0; i < errors.length; i++) {
+       console.log("Card: " + errors[i][0]);
+       console.log("Property: " + errors[i][1]);
+       console.log("Received: " + errors[i][2]);
+    }
+  }
 }
 
 // TODO: Add functionality for single leaders (WT)
 // Follows the order of operations the game uses
 function teamSim(teamArray, turnMax) {
-  if(!validateInputs(teamArray)){
-    return "Please enter a valid team"
+  error = runValidations();
+  if(error != true){
+    return error;
   }
   else {
     turnCount = 0;
