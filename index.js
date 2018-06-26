65676f07432111a299aa02e127966a7b144a7341
Tech-A67 <mnash@spartaglobal.com>
2018-06-26 17:17:09 +0100

    adjusted file structure

--- index.js
+++ index.js
@@ -1,4 +1,139 @@
-
+// TODO: Get values for each type and branch
+dupeBoosts = [
+    {
+      'TEQ': {
+        'S': {
+          //EXAMPLE 'free':[1000, 3, 5, 0, 5],
+          // 'br':[AttackBoost, SABoost, AAChanceMax, CritChanceMax, TotalChanceMax],
+          'bl':,
+          'tr':,
+          'tl':,
+  
+          },
+        'A': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 5400
+        },
+        'B': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 3240
+          }
+        },
+      'AGL': {
+        'S': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 7000
+          },
+        'A': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 5400
+        },
+        'B': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 3000
+          }
+        },
+      'STR': {
+        'S': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 7560
+          },
+        'A': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 5400
+        },
+        'B': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 3240
+          }
+        },
+      'PHY': {
+        'S': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 7000
+          },
+        'A': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 5000
+        },
+        'B': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 3000
+          }
+        },
+      'INT': {
+        'S': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 7000
+          },
+        'A': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 5000
+        },
+        'B': {
+          'free':,
+          'br':,
+          'bl':,
+          'tr':,
+          'tl': 3000
+          }
+        },
+  
+    }
+  ];
+// Triggered by the user clicking the Run button. 
+// Starts the simulation
+function runButton(){
+    // TODO: Pull the value of the sim-mode dropdown to decide which to run.
+    if(sim-mode === 'single')
+    {
+      singleCardSim();
+    }
+    else
+    {
+      setTeam();
+      teamSim(team);
+    }
+  }
 var simDropdown = document.getElementById('sim-mode');
 
 function displaySim() {
@@ -7,3 +142,27 @@ function displaySim() {
     }
     
 }
+
+
+//TODO: push the selected cards to team
+// Clears then sets the team to equal the users choices
+// Note: Leaders should always be placed at 0 and 6 for leaderSkills() to work
+function setTeam() {
+    team = [];
+    team.push();
+    return team;
+  }
+  
+  // TODO: fill in all the values
+  // Adds the required K,V pairs to each card in the team
+  function prepCards(teamArray){
+    teamArray.forEach(card => 
+      { card['ki'] = 0;
+        card['SALvl'] = 1;
+        card['maxAttack'] = card['maxAttack'] + HTMLReadPotential;
+        card[''] = ;
+        card[] = ;
+        card[] = ;
+        card[] = ;
+      })
+  }
---
+++ team_calculator.js
@@ -0,0 +1,189 @@
+/*
+Dokkan Simulator Library
+Call 
+teamSim([ArrayContainingTeam], DesiredTurnSimCount)
+or 
+
+*/
+
+// TODO: Add validation of current inputs
+// Returns a boolean for if the current inputs are valid
+function validateInputs(teamArray) {
+  if(teamArray.length !== 7){
+    return "The array is not the length required for the simulator. Requires: 7, Received: " + teamArray.length;
+  }
+  teamArray.forEach(card => {
+    if (card.turnStart()){
+       card.ki; 
+      }
+  });
+}
+
+// TODO: Add functionality for single leaders (WT)
+// Follows the order of operations the game uses
+function teamSim(teamArray, turnMax) {
+  if(!validateInputs(teamArray)){
+    return "Please enter a valid team"
+  }
+  else {
+    turnCount = 0;
+    prepCards(teamArray);
+    setRotations(teamArray);
+    leaderSkills(teamArray);
+    while(turnCount < turnMax) {
+      currentRotation = currentRotationGetter(teamArray, turnCount);
+      turnStartSkills(currentRotation);
+      linkSkillsActivate(currentRotation);
+      onAttackSkills(currentRotation);
+      onSASkills(currentRotation);
+      simTurn(currentRotation);
+      afterAttackSkills(currentRotation);
+      afterSASkills(currentRotation);
+      turnCount++;
+    }
+  }
+}
+
+
+// TODO: push the selected characters to their respective rotations
+// Sets the rotation arrays to be equal to the users chosen cards/order
+function setRotations(teamArray) {
+  firstRotation = [];
+  secondRotation = [];
+  floaters = [];
+  firstRotation.push();
+  secondRotation.push();
+  floaters.push();
+}
+
+// TODO: redesign to allow for 1 leader (WT)
+// Checks for the type of leaderskill each of the designated leaders has and runs that on the team.
+function leaderSkills(teamArray) {
+  if (teamArray[0].percLeader){ teamArray[0].percLeader(); }
+  if (teamArray[6].percLeader){ teamArray[6].percLeader(); }
+  if (teamArray[0].flatLeader){ teamArray[0].flatLeader(); }
+  if (teamArray[6].flatLeader){ teamArray[6].flatLeader(); }
+}
+
+
+// Uses modular maths to find the current rotation and floater depending upon the current turnCount.
+function currentRotationGetter(teamArray, turnCount){
+  currentRotation = [];
+    if (turnCount % 2 === 0){
+        currentRotation = currentRotation.concat(teamArray[0]).concat(teamArray[1]).concat(teamArray[(turnCount % 3) + 4]);
+    }
+    else {
+        currentRotation = currentRotation.concat(teamArray[2]).concat(teamArray[3]).concat(teamArray[(turnCount % 3) + 4]);
+    }
+    return currentRotation;
+}
+
+// Checks each card in the current rotation to see if they have a turnStart skill. Will run if they do.
+function turnStartSkills(currentRotation) {
+  currentRotation.forEach(card => {
+    if (card.turnStart()){ card.turnStart(); }
+  });
+}
+
+
+// TODO
+// Checks the adjacent cards for equivalent Ki links and activates shared links.
+function linkSkillsActivate(currentRotation) {
+
+}
+
+// Checks then activates onAttack skills for the current rotation
+function onAttackSkills(currentRotation) {
+  currentRotation.forEach(card => {
+    if (card.onAttack()){ card.onAttack(); }
+  });
+}
+
+// Checks then activates onSA skills for the current rotation
+function onSASkills(currentRotation) {
+  currentRotation.forEach(card => {
+    if (card.onSA()){ card.onSA(); }
+  });
+}
+
+// TODO
+// Returns the total damage integer for the turn after simulating the 
+// turn for either of the possible positions of the main rotation 
+function simAttacks(currentRotation) {
+  var turnDamage = 0;
+  for (let i = 1; i <= 2; i++) {
+    var currentCalc = 0;
+    currentRotation.forEach(card => {
+      if (card.currentKi >= card.minSAKi)
+        currentCalc += SACalc(card);
+      else
+      currentCalc += card.attackCalc();
+    })
+    if (currentCalc > turnDamage)
+    {
+      turnDamage = currentCalc;
+    }
+    swapRotation(teamArray);
+  }
+  return turnDamage;
+}
+
+// Swaps the positions of the first and second card in the current rotation.
+// Used in turnSim() to see if more damage can be achieved in the current turn
+function swapRotation(currentRotation) {
+  [currentRotation[0], currentRotation[1]] = [currentRotation[1], currentRotation[0]];
+}
+
+// TODO
+// Returns the attack for a card given its current stats
+function SACalc(card){
+
+}
+
+// TODO
+function attackCalc(card){
+    
+}
+
+// Returns an array with 3 numbers. Simulates a random amount of Ki per turn.
+function randomKiGen() {
+  // First number is the amount of ki, second is the chance of that number appearing. 
+  // Adjust as needed.
+  weighting = { 1:0.1, 2:0.2, 3:0.3, 4:0.2, 5:0.1, 6:0.05, 7:0.025, 8:0.025 };
+  var i;
+  var j;
+  table=[];
+  for (i in weighting) {
+    for (j = 0; j < weighting[i] * 10; j++) {
+      table.push(i);
+    }
+  }
+  var k = 0;
+  kiTable = [];
+  while (k < 3) {
+    kiTable.push(table[Math.floor(Math.random() * table.length)]);
+    k++;
+  }
+  return kiTable;
+}
+
+// Checks the given card for any afterAttack skills and runs them
+function afterAttackSkills(card) {
+  if (card.afterAttack()) {
+    card.afterAttack(); 
+  }
+}
+
+// Checks the given card for any afterSASkills skills and runs them
+function afterSASkills(card) {
+  if (card.afterSA()) { 
+    card.afterSA();
+  }
+}
+
+// TODO
+// Simulates the attack of a single card
+function singleCardSim() {
+
+}
+
